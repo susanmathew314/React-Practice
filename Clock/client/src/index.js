78fba557826fb7b4f2fdbe0f03df05ec1430d954
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-12-21 15:44:39 -0600

    added numbers, centered clock on page

--- Clock/client/src/index.js
+++ Clock/client/src/index.js
@@ -3,53 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-//need to implement: AM/PM color change, 'numbers'
-
-
 class Clock extends React.Component {
 	renderHour(h){
 		h = h <= 12 ? h : h-12;
 		var styles = {
 			borderColor:'hsl(' + 360/12*h + ',100%,50%)',
-				
 		}
-		return (
-			
-			<div style={styles} className="hour"> </div>
-		)
+		return (<div style={styles} className="hour"> </div>);
 	}
 	renderMin(m){
 		var styles = {
 			borderColor:'hsl(' + 360/60*m + ',100%,50%)',
-				
 		}
-		return (<div style={styles} className="min"> </div>);
+		return (<div className="min"><div style={styles}> </div></div>);
 	}
 	renderSec(s){
 		var styles = {
 			borderColor:'hsl(' + 360/60*s + ',100%,50%)',
-				
 		}
-		return (<div style={styles} className="sec"> </div>);
+		return (<div  className="sec"><div style={styles}> </div></div>);
+	}
+	renderNumber(i){
+		var styles = {
+			borderColor:'hsl(' + 360/12*i + ',100%,50%)',
+		}
+		return(<div className={"num n"+i}><div style={styles}>{i}</div></div>)
 	}
 	render() {
 		const time = new Date();
-		console.log(time);
 		return (
-			<div className="clock">
-				{this.renderHour(time.getHours())}
-				{ this.renderMin(time.getMinutes())}
-				{ this.renderSec(time.getSeconds())}
-				<p className="output">{time.toLocaleTimeString()}</p>
+			<div>
+				<h1>Rainbow Clock!</h1>
+				<div className="clock">
+					{this.renderHour(time.getHours())}
+					{ this.renderMin(time.getMinutes())}
+					{ this.renderSec(time.getSeconds())}
+					{[...Array(12).keys()].map(i=>this.renderNumber(++i))}
+					<p className="output">{time.toLocaleTimeString()}</p>
+				</div>
 			</div>
-			
 		)
 	}
 }
 
 
 setInterval(()=>ReactDOM.render(<Clock />,document.getElementById("app")),1);
-
 
 
 
