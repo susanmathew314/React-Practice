d9a057551f9650042aaa9be098376b0f711f2050
Alyssa McKee <alyssa_mckee@yahoo.com>
2018-01-04 17:53:44 -0600

    implemented adding items, started on filtering

--- Todo/src/index.js
+++ Todo/src/index.js
@@ -18,57 +18,103 @@ class List extends React.Component{
 		super(props);
 		this.state = {
 			items: [
-				{id: 2, completed: false, text: "implement input"},
-				{id: 3, completed: false, text: "implement filters"},
-				{id: 4, completed: false, text: "implement edit"},
-				{id: 5, completed: false, text: "implement delete"},
-				{id: 1, completed: true, text: "helloworld"},
+				{id: 3, completed: false, text: "delete item"},
+				{id: 2, completed: false, text: "filter item"},
+				{id: 1, completed: true, text: "add item"},
+				{id: 0, completed: true, text: "helloworld"},
 			],//{completed: true, text: "helloworld", editing: false}
-			filter: null,
+			filter: "all",
 		}
-		this.nextId = 5;
+		this.nextId = this.state.items.length;
 		this.onChangeCompleted = this.onChangeCompleted.bind(this);
+		this.onSubmitNew = this.onSubmitNew.bind(this);
+		this.onfilter = this.onfilter.bind(this);
 	}
 	onChangeCompleted(id){
 		var item = this.state.items.filter((item)=>item.id===id)[0];
 		console.log(item);
 		item.completed = !item.completed;
-		const completed = this.state.items.filter((item)=>item.completed)//.sort((a,b)=>a.id > b.id)
-		const uncompleted = this.state.items.filter((item)=>!item.completed).sort((a,b)=>a.id > b.id)
-		var newList = uncompleted.concat(completed);
+		const newList = this.state.items.sort( (a,b) => a.completed ===true && a.completed === b.completed ? 0:  a.completed === true? 1 : b.completed === true? -1 : a.id < b.id);
 		this.setState({items: newList});
+	}
+	onSubmitNew(text){
+		var newItem = {id: this.nextId++, completed: false, text: text}
+		const items = this.state.items.slice();
+		items.unshift(newItem);
+		this.setState({items: items});
+	}
+	onfilter(filter){
+		this.setState({filter: filter});
 	}
 	render(){
 		
 		return(
 		<div className="list-container">
-			<ListInput />
+			<ListInput onSubmitNew={this.onSubmitNew } />
 			{this.state.items.map((item)=>
-				<ListItem onChangeCompleted={this.onChangeCompleted} key={item.id} id={item.id} completed={item.completed} text={item.text}/>
+				<ListItem onSubmitNew={this.onSubmitNew} onChangeCompleted={this.onChangeCompleted} key={item.id} id={item.id} completed={item.completed} text={item.text}/>
 			)}
+			<div className="list-footer">
+				<p className="uncompleted-count">{this.state.items.filter((i)=>!i.completed).length + " items left"}</p>
+				<div>
+				<button className={this.state.filter === "all" ? "selected" : "unselected"}
+					onClick={()=>{this.onfilter("all")}}
+					>all</button>
+				<button className={this.state.filter === "completed" ? "selected" : "unselected"}
+					onClick={()=>{this.onfilter("completed")}}
+					>completed</button>
+				<button className={this.state.filter === "incomplete" ? "selected" : "unselected"}
+					onClick={()=>{this.onfilter("incomplete")}}
+					>incomplete</button>
+				</div>
+				<p>{this.state.items.filter((i)=>i.completed).length != 0? "remove completed": ""}</p>
+			</div>
 		</div>
 		)
 	}
 }
-function ListInput(props){
-	return (
-		<form className="newitem" action="javascript:void(0);">
-			<input type="text" name="newitem" placeholder="add a new item"/>
-		</form>
-	)
-	
+class ListInput extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			value:"",
+		}
+		this.handleChange = this.handleChange.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
+	}
+	handleChange(e){
+		this.setState({value: e.target.value});
+	}
+	handleSubmit(e){
+		e.preventDefault();
+		const text = this.state.value;
+		this.setState({value:""})
+		if (text!== "")
+			this.props.onSubmitNew(text);
+	}
+	render(){
+		return (
+			<form className="newitem" action="javascript:void(0);" onSubmit={this.handleSubmit}>
+				<button type="submit">v</button>
+				<input onChange={this.handleChange}type="text" value={this.state.value} name="newitem" placeholder="add a new item"/>
+			</form>
+		)
+	}
 }
-function ListItem(props){
-	var classes = ["item"];
-	if (props.completed)
-		classes.push("completed");
+class ListItem extends React.Component{
 	
-	return (
-		<form>
-			<input type="checkbox" name="status" checked={props.completed} onChange={()=> props.onChangeCompleted(props.id)}/>
-			<label className={classes.join(" ")} >{props.text}</label>
-		</form>
-	)
+	render(){
+		var classes = ["item"];
+		if (this.props.completed)
+			classes.push("completed");
+		
+		return (
+			<form>
+				<input type="checkbox" name="status" checked={this.props.completed} onChange={()=> this.props.onChangeCompleted(this.props.id)}/>
+				<label className={classes.join(" ")} >{this.props.text}</label>
+			</form>
+		)
+	}
 }
 class EditItem extends React.Component{
 	
